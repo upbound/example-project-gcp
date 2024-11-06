cfdd887a2578ab6fcb3eed75543047f57e871eb6
Adam Wolfe Gordon <awg@upbound.io>
2024-11-06 15:33:07 -0700

    Follow Crossplane best practices

--- functions/compose-bucket-python/main.py
+++ functions/compose-bucket-python/main.py
@@ -1,14 +1,15 @@
 from crossplane.function import resource
 from crossplane.function.proto.v1 import run_function_pb2 as fnv1
-from .model.io.k8s.apimachinery.pkg.apis.meta import v1 as metav1
-from .model.io.upbound.gcp.storage.bucket import v1beta1 as bucketv1beta1
-from .model.io.upbound.gcp.storage.bucketacl import v1beta1 as aclv1beta1
-from .model.com.example.platform.xstoragebucket import v1alpha1
+from model.io.k8s.apimachinery.pkg.apis.meta import v1 as metav1
+from model.io.upbound.gcp.storage.bucket import v1beta1 as bucketv1beta1
+from model.io.upbound.gcp.storage.bucketacl import v1beta1 as aclv1beta1
+from model.com.example.platform.xstoragebucket import v1alpha1
 
 def compose(req: fnv1.RunFunctionRequest, rsp: fnv1.RunFunctionResponse):
     observed_xr = v1alpha1.XStorageBucket(**req.observed.composite.resource)
     xr_name = observed_xr.metadata.name
     bucket_name = xr_name + "-bucket"
+    params = observed_xr.spec.parameters
 
     bucket = bucketv1beta1.Bucket(
         apiVersion="storage.gcp.upbound.io/v1beta1",
@@ -18,9 +19,9 @@ def compose(req: fnv1.RunFunctionRequest, rsp: fnv1.RunFunctionResponse):
         ),
         spec=bucketv1beta1.Spec(
             forProvider=bucketv1beta1.ForProvider(
-                location=observed_xr.spec.location,
+                location=params.location,
                 versioning=[bucketv1beta1.VersioningItem(
-                    enabled=observed_xr.spec.versioning,
+                    enabled=params.versioning,
                 )],
             ),
         ),
@@ -38,7 +39,7 @@ def compose(req: fnv1.RunFunctionRequest, rsp: fnv1.RunFunctionResponse):
                 bucketRef=aclv1beta1.BucketRef(
                     name=bucket_name,
                 ),
-                predefinedAcl=observed_xr.spec.acl,
+                predefinedAcl=params.acl,
             ),
         ),
     )
